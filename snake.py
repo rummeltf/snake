f5937f4a7995ab993835d768fcafacc7960c3744
Thomas Rummel <93659558+rummeltf@users.noreply.github.com>
2022-01-19 16:55:25 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 
-
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 move_dist = 20
 
@@ -13,12 +12,18 @@ class Snake:
 
     def create_snake(self):
         for position in starting_positions:
+            self.add_segment(position)
+
+    def add_segment(self, position):
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.speed("fastest")
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
