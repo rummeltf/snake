4e5ff6be5e5cee4a0f10f0713b9756532212670c
Thomas Rummel <93659558+rummeltf@users.noreply.github.com>
2022-02-07 16:50:14 -0500

    high scoring and resetting implemented

--- main.py
+++ main.py
@@ -13,13 +13,6 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 score = Score()
-game_over = Turtle()
-
-game_over.penup()
-game_over.hideturtle()
-game_over.goto(0, 0)
-game_over.color("white")
-
 
 screen.listen()
 screen.onkey(snake.up, "w")
@@ -37,20 +30,22 @@ while game_is_on:
         snake.extend()
         score.clear()
         score.score += 1
-        score.write(arg=f"Score: {score.score}", move=False, align="center", font=('Times New Roman', 20, "normal"))
+        score.update_score()
     
     # detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        game_over.write(arg="Game Over", move=False, align="center", font=('Times New Roman', 30, "normal"))
+        score.reset()
+        snake.reset()
+        score.update_score()
 
     # detect collision with self
-    for segment in snake.segments:
-        if snake.head.distance(segment) < 20:
-            if snake.head.distance(segment) < 5:
-                continue
-            else:
-                game_is_on = False
-                game_over.write(arg="Game Over", move=False, align="center", font=('Times New Roman', 30, "normal"))
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 15:
+            score.reset()
+            snake.reset()
+            score.update_score()
+
+
+
 
 screen.exitonclick()
